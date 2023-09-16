23ad19d13b8de1361f56ec29a849ae9aa421e178
noname <nomail@nomail.com>
2023-09-16 21:57:39 +0530

    Authentication Views implemented.

--- Backend/frontend/src/App.js
+++ Backend/frontend/src/App.js
@@ -1,40 +1,47 @@
 import axios from "axios";
-import React, { useState, useEffect } from 'react'
-
-
+import React, { useState, useEffect } from "react";
+import NavAnon from "./components/NavAnon";
+import NavStu from "./components/NavStu";
+import Home from "./components/Home"
+import Courses from "./components/Courses"
+import Faq from "./components/Faq";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import "./App.css";
 
 function App() {
-
-  const [details, setDetails] = useState([])
+  const [details, setDetails] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     // Axios GET request to fetch data from the API
-    axios.get('http://127.0.0.1:8000/currentuser')
+    axios
+      .get("http://127.0.0.1:8000/currentuser")
       .then((res) => {
         setDetails(res.data);
         setIsLoading(false); // Data is loaded
       })
       .catch((err) => {
-        console.error('Error fetching data:', err);
+        console.error("Error fetching data:", err);
         setIsLoading(false); // Loading failed
       });
   }, []);
 
+  return (
+    
+    <>
+    <Router>
+      <div className="app">{details.id ? <NavStu /> : <NavAnon />}</div>
 
-return (
-  <div className="app">
-    <h1>Django Data</h1>
-    <hr />
-    {isLoading ? (
-        <p>Loading...</p>
-      ) : (
-        <>
-        <p>Currently Logged in User:</p>{details.username}
-      </>
-      )}
-  </div>
-);
+      <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/course" element={<Courses />} />
+          <Route path="/faq" element={<Faq />} />
+          <Route path="/check" element={<Home />} />
+      </Routes>
+
+    </Router>
+    </>
+  );
 }
 
 export default App;
--- Backend/frontend/src/components/NavStu.js
+++ Backend/frontend/src/components/NavStu.js
@@ -1,18 +1,41 @@
-import React from 'react'
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 
 export default function NavStu() {
+  const [details, setDetails] = useState([]);
+  useEffect(() => {
+    // Axios GET request to fetch data from the API
+    axios
+      .get("http://127.0.0.1:8000/currentuser")
+      .then((res) => {
+        setDetails(res.data);
+      })
+      .catch((err) => {
+        console.error("Error fetching data:", err);
+      });
+  }, []);
+
   return (
     <div>
       <header>
         <nav>
-            <ul>
-                <li><a href="/">Home</a></li>
-                <li><a href="/course">Courses</a></li>
-                <li><a href="/faq">FAQ</a></li>
-                <li class="dashboard"><a href="/dashboard">Dashboard</a></li>
-            </ul>
+          <ul>
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/course">Courses</a>
+            </li>
+            <li>
+              <a href="/faq">FAQ</a>
+            </li>
+            <li class="dashboard">
+              <a href="/dashboard">Dashboard</a>
+            </li>
+            <li className="userdisp">{details.username}</li>
+          </ul>
         </nav>
-    </header>
+      </header>
     </div>
-  )
+  );
 }
