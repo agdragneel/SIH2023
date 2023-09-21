e0bac7f0a60e9a0509cc6b261d8da4d5f015d7fa
noname <nomail@nomail.com>
2023-09-22 02:05:53 +0530

    Ready for Show

--- Backend/frontend/src/components/Courses.js
+++ Backend/frontend/src/components/Courses.js
@@ -7,76 +7,75 @@ const classes = [
     className: 'LKG',
     subjects: ['Math', 'Science', 'English'],
     descriptions: {
-      Math: 'Math description for LKG',
-      Science: 'Science description for LKG',
-      English: 'English description for LKG',
+      Math: 'In Lower Kindergarten (LKG), children are introduced to the exciting world of mathematics through a series of interactive and play-based activities. They embark on their mathematical journey by learning to count, recognize basic shapes, and understand the concept of quantity. LKG Math aims to create a strong foundation for future mathematical learning by fostering curiosity, problem-solving skills, and a positive attitude toward numbers. Students engage in games, puzzles, and fun exercises that make learning math an enjoyable and enriching experience.',
+      Science: 'Lower Kindergarten (LKG) Science is a program designed to ignite the natural curiosity of young learners about the world around them. Through hands-on exploration, simple experiments, and age-appropriate activities, children are introduced to the wonders of the natural world. LKG Science focuses on fundamental concepts such as living and non-living things, basic weather patterns, and the properties of common materials. The aim is to develop observation skills, encourage questions, and inspire a lifelong interest in scientific inquiry from an early age.',
+      English: 'Lower Kindergarten (LKG) English is dedicated to nurturing essential language skills in young learners. Through a language-rich environment, students are encouraged to actively listen, speak, and express themselves. LKG English lays the foundation for language fluency and literacy by immersing children in stories, songs, and interactive activities. This early exposure to language not only builds vocabulary but also cultivates a love for reading and communication. It provides the building blocks for effective communication, setting the stage for future language development'
     },
     imgurl: 'https://images.unsplash.com/photo-1578349035260-9f3d4042f1f7?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=400&ixid=MnwxfDB8MXxyYW5kb218MHx8a2luZGVyZ2FydGVuLHNjaG9vbHx8fHx8fDE2OTUwMjYwNTA&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=400',
-    classdesc: 'Class Description for LKG'
+    classdesc: 'LKG introduces young children to the world of learning through play-based activities, laying the groundwork for future education'
   },
   {
     className: 'I',
     subjects: ['Math', 'Science', 'Social Studies'],
     descriptions: {
-      Math: 'Math description for Class I',
-      Science: 'Science description for Class I',
+      Math: 'In Class I, students embark on their mathematical journey by learning fundamental arithmetic concepts and number recognition. This foundational year in mathematics focuses on addition, subtraction, and the development of problem-solving skills. Through interactive lessons and practical examples, students build a strong mathematical base, setting the stage for more complex mathematical concepts in the years to come. Class I Math encourages a positive attitude toward math and lays the groundwork for future mathematical success.',
+      Science: 'Class I Science is an exciting exploration of the natural world designed to spark curiosity in young learners. Through hands-on experiments, observations, and age-appropriate lessons, students discover basic scientific principles. Topics include plants, animals, and the environment, fostering a sense of wonder and scientific inquiry. The goal is to nurture a budding interest in science and cultivate observation skills from an early age, setting the stage for a lifelong appreciation of the natural world.',
       'Social Studies': 'Social Studies description for Class I',
+      Social_studies:'Class I Social Studies is an introduction to the broader world and basic social concepts. Students learn about their immediate surroundings, community, and their place within it. This foundational year in social studies focuses on building a sense of belonging and awareness by exploring topics such as people, places, and social interactions. Class I Social Studies encourages students to understand and appreciate their community while laying the groundwork for more in-depth social studies exploration in the future.'
     },
     imgurl: 'https://images.unsplash.com/photo-1564429238817-393bd4286b2d?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=400&ixid=MnwxfDB8MXxyYW5kb218MHx8a2luZGVyZ2FydGVuLHNjaG9vbHx8fHx8fDE2OTUwMjYxMDE&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=400',
-    classdesc: 'Class Description for I'
+    classdesc: 'Class I is the starting point of formal education, where students learn basic numeracy and literacy skills in an interactive environment. '
 
   },
   {
     className: 'II',
-    subjects: ['Math', 'Science', 'Social Studies'],
+    subject: ['Math', 'Science', 'Social Studies'],
     descriptions: {
-      Math: 'Math description for Class II',
-      Science: 'Science description for Class II',
-      'Social Studies': 'Social Studies description for Class II',
+      Math: 'Class II Math builds upon the foundational math skills acquired in Class I. Students delve into more complex mathematical concepts, including multiplication, division, and the exploration of geometric shapes. Through engaging lessons and practical applications, students continue to develop their problem-solving abilities and mathematical fluency. Class II Math aims to provide students with a solid mathematical foundation, preparing them for more advanced mathematical challenges in the years ahead.',
+      Science: 'Class II Science expands students horizons in the realm of scientific exploration. They dive into a broader range of scientific topics, including ecosystems, weather, and matter. Through hands-on experiments and age-appropriate investigations, students further develop their scientific curiosity and critical thinking skills. Class II Science encourages students to question, observe, and explore, fostering a deeper understanding of the natural world and its wonders.',
+      Social_Studies: 'Class II Social Studies broadens students understanding of the world around them. They explore geography, culture, and communities, fostering an appreciation for diversity and global awareness. Topics include different regions, customs, and the interconnectedness of societies. Class II Social Studies encourages students to embrace the richness of our global community while developing their social studies foundation',
     },
     imgurl: 'https://images.unsplash.com/photo-1581726690015-c9861fa5057f?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=400&ixid=MnwxfDB8MXxyYW5kb218MHx8a2luZGVyZ2FydGVuLHNjaG9vbHx8fHx8fDE2OTUwMjY0MDk&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=400',
-    classdesc: 'Class Description for II'
+    classdesc: 'In Class II, students continue to develop their foundational math and language skills, building a strong academic base.'
+   
 
   },
   {
     className: 'III',
     subjects: ['Math', 'Science', 'Social Studies', 'History'],
     descriptions: {
-      Math: 'Math description for Class III',
-      Science: 'Science description for Class III',
-      'Social Studies': 'Social Studies description for Class III',
-      History: 'History description for Class III',
+      Math: 'Class III Math marks a significant step forward in mathematical learning as students delve deeper into arithmetic and problem-solving. They master fundamental skills, including multiplication, division, and fractions, while also honing their ability to tackle more complex math challenges. Class III Math aims to equip students with a solid mathematical foundation that will serve them well in their academic journey, emphasizing both mathematical proficiency and confidence.',
+      Science: 'Class III Science extends students\' scientific knowledge by exploring various branches of science, including life sciences, physical sciences, and earth sciences. They delve into topics such as life cycles, forces, and energy transformations. Through hands-on experiments and age-appropriate investigations, students continue to develop their scientific inquiry skills, fostering a deeper appreciation for the wonders of the natural world.',
+      'Social Studies': 'Class III Social Studies takes students on a journey through history, geography, and the principles of governance. They gain a broader perspective on the world and their place in it. Topics include ancient civilizations, map reading, and an introduction to the concepts of democracy and citizenship. Class III Social Studies encourages students to explore the rich tapestry of human history and society, promoting a sense of global awareness and responsible citizenship',
+      History: 'Class III History delves into key historical events and figures, providing students with a deeper understanding of the past and its influence on the present. They explore pivotal moments in history, including ancient civilizations, important inventions, and the stories of inspiring individuals. Class III History aims to cultivate historical awareness and critical thinking skills, encouraging students to appreciate the role of history in shaping our world.',
     },
     imgurl: 'https://images.unsplash.com/photo-1471970471555-19d4b113e9ed?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=400&ixid=MnwxfDB8MXxyYW5kb218MHx8a2luZGVyZ2FydGVuLHNjaG9vbHx8fHx8fDE2OTUwMjYzMDQ&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=400',
-    classdesc: 'Class Description for III'
-
-  },
+    classdesc: 'Class III marks the beginning of more in-depth studies, encompassing math, science, social studies, and expanding knowledge in various subjects.'},
   {
     className: 'IV',
     subjects: ['Math', 'Science', 'Social Studies', 'History'],
     descriptions: {
-      Math: 'Math description for Class IV',
-      Science: 'Science description for Class IV',
-      'Social Studies': 'Social Studies description for Class IV',
-      History: 'History description for Class IV',
+      Math: 'In Class IV, students continue their mathematical journey by refining their skills and tackling more complex mathematical concepts. Topics include decimals, geometry, and problem-solving strategies. Class IV Math aims to equip students with the mathematical proficiency and problem-solving abilities needed to excel in more advanced math studies in the years ahead.',
+      Science: 'Class IV Science deepens students\' understanding of scientific principles by exploring a range of topics, including ecosystems, matter, and energy transformations. They engage in hands-on experiments and investigations that encourage critical thinking and scientific inquiry. Class IV Science encourages students to connect the dots between different scientific disciplines, fostering a holistic understanding of the natural world.',
+      'Social Studies': 'Class IV Social Studies offers students a broader perspective on history, geography, and the diversity of cultures and societies around the world. They explore topics such as world geography, ancient civilizations, and cultural traditions. Class IV Social Studies aims to nurture global awareness, appreciation for cultural diversity, and a sense of interconnectedness among students.',
+      History: 'Class IV History delves into significant historical periods and their impact on society and the world. Students study key events, important figures, and the evolution of civilizations. Through engaging lessons and historical narratives, Class IV History encourages students to analyze historical contexts and draw connections between the past and the present.',
     },
     imgurl: 'https://images.unsplash.com/photo-1554721299-e0b8aa7666ce?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=400&ixid=MnwxfDB8MXxyYW5kb218MHx8a2luZGVyZ2FydGVuLHNjaG9vbHx8fHx8fDE2OTUwMjYzMzI&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=400',
-    classdesc: 'Class Description for IV'
+    classdesc: 'Class IV focuses on furthering academic skills and introduces students to more complex topics in math, science, and social studies.'
 
   },
   {
     className: 'V',
     subjects: ['Math', 'Science', 'Social Studies', 'History'],
     descriptions: {
-      Math: 'Math description for Class V',
-      Science: 'Science description for Class V',
-      'Social Studies': 'Social Studies description for Class V',
-      History: 'History description for Class V',
+      Math: 'Class V Math focuses on further refining mathematical skills and introducing students to more advanced topics, such as fractions, decimals, and geometry. Students also deepen their problem-solving abilities and mathematical fluency. Class V Math prepares students for the challenges of more advanced mathematical studies while instilling confidence in their mathematical capabilities.',
+      Science: 'Class V Science explores a wide range of scientific disciplines, allowing students to discover the interconnectedness of the natural world. They investigate topics such as earth science, astronomy, and the diversity of life forms. Through hands-on experiments and in-depth explorations, Class V Science encourages students to embrace their roles as budding scientists, promoting curiosity and inquiry.',
+      'Social Studies': 'Class V Social Studies immerses students in the study of world cultures, geography, and global issues. They delve into the rich tapestry of human civilization, exploring the interplay of cultures, history, and geography. Class V Social Studies fosters global awareness, critical thinking, and a sense of responsibility in addressing global challenges.',
+      History: 'Class V History examines historical events and their enduring significance, encouraging students to think critically about the past. Students study major historical periods, revolutions, and key figures who have shaped history. Class V History aims to deepen historical awareness and analytical thinking, enabling students to appreciate the complexities of our shared human story.',
     },
     imgurl: 'https://images.unsplash.com/photo-1600792174277-8d734290a61f?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=400&ixid=MnwxfDB8MXxyYW5kb218MHx8a2luZGVyZ2FydGVuLHNjaG9vbHx8fHx8fDE2OTUwMjY0NDQ&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=400',
-    classdesc: 'Class Description for IV'
 
-  },
+    classdesc: 'Class V emphasizes critical thinking and a deeper understanding of subjects, preparing students for more advanced studies in the future.',},
 ];
 
 export default function Courses() {
--- Backend/frontend/src/components/Home.js
+++ Backend/frontend/src/components/Home.js
@@ -3,10 +3,10 @@ import './Home.css';
 
 export default function Home() {
   const [translatedText, setTranslatedText] = useState(null);
-  const [isTranslatedToHindi, setIsTranslatedToHindi] = useState(false);
+  const [selectedLanguage, setSelectedLanguage] = useState('English'); // Default language is English
 
-  // JavaScript to trigger animation for the About the Company section and images
   useEffect(() => {
+    // JavaScript to trigger animation for the About the Company section and images
     const companyInfo = document.querySelector('.company-info');
     const companyPhotos = document.querySelectorAll('.company-photos img');
 
@@ -19,38 +19,62 @@ export default function Home() {
     });
   }, []);
 
-  // Function to handle text translation and update the state
-  const translateToHindi = () => {
-    // This is a simple example of hardcoded translations.
-    // Replace these with your actual translations as needed.
-    const translations = {
+  const translations = {
+    Hindi: {
       'Welcome to RoamEd': 'रोमएड में आपका स्वागत है',
       'Learn at your own pace, anytime, anywhere.':
         'अपने खुद के गति पर पढ़ें, कभी भी, कहीं भी।',
       'Education in motion.': 'शिक्षा गतिविधि में।',
       'Get Started': 'शुरू करें',
-      'About RoamEd': 'रोमएड के बारे में',
+      'About RoamEd':
+        'रोमएड के बारे में',
       'At RoamEd, we are dedicated to making a positive impact on the lives of tribal children who face unique challenges due to their nomadic lifestyles. Our mission is to provide a top-notch educational platform that empowers these young learners to access quality education, regardless of their geographic mobility.':
         'रोमएड में, हम उन जनजातीय बच्चों के जीवन पर सकारात्मक प्रभाव डालने के समर्पित हैं जो अपनी घुमंतु जीवनशैली के कारण विशेष चुनौतियों का सामना करते हैं। हमारा उद्देश्य एक उच्च-कक्षा की शिक्षा प्लेटफार्म प्रदान करना है जो इन युवा शिक्षार्थियों को उच्च गुणवत्ता की शिक्षा तक पहुँचाने की शक्ति प्रदान करता है, उनके भूगोलिक चलन की अनवरत स्वतंत्रता के बावजूद।',
       "Our platform is designed to be flexible and accessible, allowing tribal children to continue their studies seamlessly while relocating. We're committed to bridging the educational gap and empowering these young learners for a brighter future.":
         "हमारा प्लेटफार्म लचीला और पहुँचने योग्य डिज़ाइन किया गया है, जिससे जनजाति के बच्चे बिना बाधा डाले अपनी पढ़ाई जारी रख सकते हैं। हम शिक्षा के अंतर को समापन करने और इन युवा शिक्षार्थियों को एक उज्ज्वल भविष्य के लिए सशक्त करने के प्रति प्रतिबद्ध हैं।",
       '“Education is the most powerful weapon which you can use to change the world." - Nelson Mandela':
         '“शिक्षा वह सबसे शक्तिशाली हथियार है जिसका आप दुनिया को बदलने के लिए उपयोग कर सकते हैं." - नेल्सन मंडेला',
-    };
+    },
+    Urdu: {
+      'Welcome to RoamEd': 'روم ایڈ میں خوش آمدید',
+      'Learn at your own pace, anytime, anywhere.':
+        'اپنی رفتار پر کبھی بھی کہیں بھی پڑھائی کریں۔',
+      'Education in motion.': 'تعلیم حرکت میں ہے۔',
+      'Get Started': 'شروع کریں',
+      'About RoamEd': 'روم ایڈ کے بارے میں',
+      'At RoamEd, we are dedicated to making a positive impact on the lives of tribal children who face unique challenges due to their nomadic lifestyles. Our mission is to provide a top-notch educational platform that empowers these young learners to access quality education, regardless of their geographic mobility.':
+        'روم ایڈ پر ہم ان جنسی بچوں کی زندگیوں پر مثبت اثر ڈالنے کے مخصوص ہیں جو اپنی گشتی زندگیوں کی بنا پر انوکھی چیلنجز کا سامنا کرتے ہیں۔ ہمارا مشن یہ ہے کہ ایک بہترین تعلیمی پلیٹ فارم فراہم کریں جو ان نوجوان تعلیم یافتگی کا راستہ دکھاتا ہے، چاہے ان کی جغرافیائی موبیلٹی کچھ بھی ہو۔',
+      "Our platform is designed to be flexible and accessible, allowing tribal children to continue their studies seamlessly while relocating. We're committed to bridging the educational gap and empowering these young learners for a brighter future.":
+        'ہمارا پلیٹ فارم لچیلے اور رسائی پذیر ہونے کا ڈیزائن کیا گیا ہے، جو جنسی بچوں کو اپنے تعلیم کو بغیر کسی رکاوٹ کے جاری رکھنے کی اجازت دیتا ہے جب بھی وہ منتقل ہوتے ہیں۔ ہم تعلیمی فاصلے کو پورا کرنے اور ان نوجوان تعلیم یافتہ کو روشن مستقبل کے لئے مضبوطی دینے کے عزمنامہ کے ساتھ ہیں۔',
+      '“Education is the most powerful weapon which you can use to change the world." - Nelson Mandela':
+        '“تعلیم دنیا کو تبدیل کرنے کے لئے آپ جو سب سے زیادہ طاقتور ہتھیار ہے." - نیلسن منڈیلا',
+    },
+    English: {
+      'Welcome to RoamEd': 'Welcome to RoamEd',
+      'Learn at your own pace, anytime, anywhere.':
+        'Learn at your own pace, anytime, anywhere.',
+      'Education in motion.': 'Education in motion.',
+      'Get Started': 'Get Started',
+      'About RoamEd': 'About RoamEd',
+      'At RoamEd, we are dedicated to making a positive impact on the lives of tribal children who face unique challenges due to their nomadic lifestyles. Our mission is to provide a top-notch educational platform that empowers these young learners to access quality education, regardless of their geographic mobility.':
+        "At RoamEd, we are dedicated to making a positive impact on the lives of tribal children who face unique challenges due to their nomadic lifestyles. Our mission is to provide a top-notch educational platform that empowers these young learners to access quality education, regardless of their geographic mobility.",
+      "Our platform is designed to be flexible and accessible, allowing tribal children to continue their studies seamlessly while relocating. We're committed to bridging the educational gap and empowering these young learners for a brighter future.":
+        "Our platform is designed to be flexible and accessible, allowing tribal children to continue their studies seamlessly while relocating. We're committed to bridging the educational gap and empowering these young learners for a brighter future.",
+      '“Education is the most powerful weapon which you can use to change the world." - Nelson Mandela':
+        '“Education is the most powerful weapon which you can use to change the world." - Nelson Mandela',
+    },
+  };
 
-    // Reverse the translation if currently in Hindi
-    if (isTranslatedToHindi) {
-      const reversedTranslations = {};
-      Object.keys(translations).forEach((key) => {
-        reversedTranslations[translations[key]] = key;
-      });
-      setTranslatedText(reversedTranslations);
-      setIsTranslatedToHindi(false);
-    } else {
-      // Set the translation to Hindi
-      setTranslatedText(translations);
-      setIsTranslatedToHindi(true);
-    }
+  const getNextLanguage = (currentLanguage) => {
+    if (currentLanguage === 'Hindi') return 'Urdu';
+    if (currentLanguage === 'Urdu') return 'English';
+    return 'Hindi'; // Cycle back to Hindi if currentLanguage is English
+  };
+
+  const translateToNextLanguage = () => {
+    const nextLanguage = getNextLanguage(selectedLanguage);
+    setTranslatedText(translations[nextLanguage]);
+    setSelectedLanguage(nextLanguage);
   };
 
   return (
@@ -58,7 +82,7 @@ export default function Home() {
       <div>
         <div className="carousel-container-static">
           <img
-            src="https://i.ibb.co/RgJDNq0/Banner.png"
+            src="https://i.ibb.co/VSCcRJ1/Orange-Animated-Online-School-Learning-Illustrated-Banner.png"
             alt="Static Image"
             className="static-image"
           />
@@ -66,33 +90,34 @@ export default function Home() {
       </div>
 
       <section className="hero">
-        <h1>{isTranslatedToHindi ? translatedText['Welcome to RoamEd'] : 'Welcome to RoamEd'}</h1>
+        <h1>{translatedText ? translatedText['Welcome to RoamEd'] : 'Welcome to RoamEd'}</h1>
         <p>
-          {isTranslatedToHindi
+          {translatedText
             ? translatedText['Learn at your own pace, anytime, anywhere.']
             : 'Learn at your own pace, anytime, anywhere.'}
         </p>
-        <p>{isTranslatedToHindi ? translatedText['Education in motion.'] : 'Education in motion.'}</p>
-        {/* <a href="/login" className="cta-button"> */}
-          {/* {isTranslatedToHindi ? translatedText['Get Started'] : 'Get Started'} */}
-        {/* </a> */}
-        <button onClick={translateToHindi}>
-          {isTranslatedToHindi ? 'Translate to English' : 'Translate to Hindi'}
+        <p>{translatedText ? translatedText['Education in motion.'] : 'Education in motion.'}</p>
+        <button onClick={translateToNextLanguage}>
+          {selectedLanguage === 'Hindi'
+            ? 'Translate to Urdu'
+            : selectedLanguage === 'Urdu'
+            ? 'Translate to English'
+            : 'Translate to Hindi'}
         </button>
       </section>
 
       <section className="company-info">
         <div className="company-text">
-          <h2>{isTranslatedToHindi ? translatedText['About RoamEd'] : 'About RoamEd'}</h2>
+          <h2>{translatedText ? translatedText['About RoamEd'] : 'About RoamEd'}</h2>
           <p>
-            {isTranslatedToHindi
+            {translatedText
               ? translatedText[
                   'At RoamEd, we are dedicated to making a positive impact on the lives of tribal children who face unique challenges due to their nomadic lifestyles. Our mission is to provide a top-notch educational platform that empowers these young learners to access quality education, regardless of their geographic mobility.'
                 ]
               : 'At RoamEd, we are dedicated to making a positive impact on the lives of tribal children who face unique challenges due to their nomadic lifestyles. Our mission is to provide a top-notch educational platform that empowers these young learners to access quality education, regardless of their geographic mobility.'}
           </p>
           <p>
-            {isTranslatedToHindi
+            {translatedText
               ? translatedText[
                   "Our platform is designed to be flexible and accessible, allowing tribal children to continue their studies seamlessly while relocating. We're committed to bridging the educational gap and empowering these young learners for a brighter future."
                 ]
@@ -100,7 +125,7 @@ export default function Home() {
           </p>
           <blockquote>
             <p>
-              {isTranslatedToHindi
+              {translatedText
                 ? translatedText[
                     '“Education is the most powerful weapon which you can use to change the world." - Nelson Mandela'
                   ]
@@ -109,14 +134,8 @@ export default function Home() {
           </blockquote>
         </div>
         <div className="company-photos">
-          <img
-            src="https://source.unsplash.com/300x300?education"
-            alt="Company Photo 1"
-          />
-          <img
-            src="https://source.unsplash.com/300x300?books"
-            alt="Company Photo 2"
-          />
+          <img src="https://source.unsplash.com/300x300?education" alt="Company Photo 1" />
+          <img src="https://source.unsplash.com/300x300?books" alt="Company Photo 2" />
         </div>
       </section>
     </>
